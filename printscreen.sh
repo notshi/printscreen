8d7ebb2255151e3168e683cc84fd5de87edade8e
kriss <kriss@xixs.com>
2017-04-14 16:44:08 +0100

    alternative screen grab

--- printscreen.sh
+++ printscreen.sh
@@ -10,7 +10,8 @@ DIRNAME="plated/source/blog/$DATETIME-$USER"
 
 mkdir $DIRNAME
 
-gnome-screenshot -f $DIRNAME/printscreen.png # -d 1
+gnome-screenshot -f $DIRNAME/printscreen.png || import -window root $DIRNAME/printscreen.png
+
 convert $DIRNAME/printscreen.png -resize 256x256 $DIRNAME/printscreen.thumb.png
 convert $DIRNAME/printscreen.png -resize 1x1 $DIRNAME/printscreen.pixel.png
 
