45886365bb6ff93115db97bad1cdbd2964808ab1
kriss <kriss@xixs.com>
2021-01-19 10:59:24 +0000

    auto directory

--- printscreen.desktop.sh
+++ printscreen.desktop.sh
@@ -1,7 +1,16 @@
 
-cd "`dirname $0`"
 
-cp printscreen.desktop ~/.local/share/applications/
+DIRNAME=`dirname $0`
+export DIRNAME=`readlink -f $DIRNAME`
+
+cd $DIRNAME
+
+envsubst <printscreen.desktop >~/.local/share/applications/printscreen.desktop
+
+echo
+echo " CREATING DESKTOP ENTRY for printscreen "
+echo
+cat ~/.local/share/applications/printscreen.desktop
 
 update-desktop-database ~/.local/share/applications/
 
